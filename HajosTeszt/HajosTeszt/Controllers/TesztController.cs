e386f554029dec07bdfb8420271e2ae232c0a6a7
István Szorcsik <istvan.szorcsik@stud.uni-corvinus.hu>
2021-04-15 17:37:21 +0200

    API Controller

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M2(string szoveg)
+        {
+            return BadRequest("Nem jó a bemenő adat");
+        }
+    }
+}
